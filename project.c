dc581a90106713304573ddd6667eeb2d23edc4e6
muskan2205 <muskan.kumari22@s.amity.edu>
2025-05-27 22:38:29 +0530

    i have added few lines

--- project.c
+++ project.c
@@ -364,3 +364,6 @@ void printAdjMatrix() {
      for (int i = 0; i < numVertices; i++) {
         for (int j = 0; j < numVertices; j++)
             printf("%d ", adjMatrix[i][j]);
+	printf("\n");
+    }
+}
