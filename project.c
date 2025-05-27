9bf7170d05fb04d856ab72adfc6b65442b8ccd02
muskan2205 <muskan.kumari22@s.amity.edu>
2025-05-27 20:40:28 +0530

    i have added few lines

--- project.c
+++ project.c
@@ -394,3 +394,8 @@ void deleteAtBeginning() {
     free(temp);
     printf("Deleted node at beginning.\n");
 }
+void deleteAtEnd() {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
