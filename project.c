07610650a5d17d6d80db20dad2a08f716e45eaa0
muskan2205 <muskan.kumari22@s.amity.edu>
2025-05-27 18:42:28 +0530

    i have added few lines

--- project.c
+++ project.c
@@ -18,3 +18,17 @@ void reverseList();
 int main() {
     int choice, data, position;
     while (1) {
+	    printf("\n\n--- Singly Linked List Menu ---\n");
+        printf("1. Insert at Beginning\n");
+        printf("2. Insert at End\n");
+        printf("3. Insert After a Node\n");
+        printf("4. Delete by Value\n");
+        printf("5. Delete at Beginning\n");
+        printf("6. Delete at End\n");
+        printf("7. Search for Element\n");
+        printf("8. Display List\n");
+        printf("9. Count Nodes\n");
+        printf("10. Reverse List\n");
+        printf("11. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
