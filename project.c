6346ef5f06480bcc6744f8f036d1e5a80c3b670a
muskan2205 <muskan.kumari22@s.amity.edu>
2025-05-27 22:27:42 +0530

    i have added few lines

--- project.c
+++ project.c
@@ -304,3 +304,6 @@ void inputGraph() {
 }
 void printAdjMatrix() {
     printf("Adjacency Matrix:\n");
+    for (int i = 0; i < numVertices; i++) {
+        for (int j = 0; j < numVertices; j++) {
+            printf("%d ", adjMatrix[i][j]);
