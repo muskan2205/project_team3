07c4a21d2a43d3fe46272f96fc23b3cf1457b561
muskan2205 <muskan.kumari22@s.amity.edu>
2025-05-27 21:47:27 +0530

    i have added few lines

--- project.c
+++ project.c
@@ -462,3 +462,9 @@ void displayBackward() {
 int countNodes() {
     int count = 0;
     struct Node* temp = head;
+     while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
