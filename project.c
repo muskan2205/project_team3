6612bf6f337feb70f23976993dd35abb53bf2065
muskan2205 <muskan.kumari22@s.amity.edu>
2025-05-27 19:44:54 +0530

    i have added few lines

--- project.c
+++ project.c
@@ -210,5 +210,12 @@ void display() {
 }
 int countNodes() {
     int count = 0;
-    struct Node* temp = head; 
+    struct Node* temp = head;
+     while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
 
