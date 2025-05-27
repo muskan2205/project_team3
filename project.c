02e512e543bb2141247465214a673a76c57c4a20
muskan2205 <muskan.kumari22@s.amity.edu>
2025-05-27 21:37:10 +0530

    i have added few lines

--- project.c
+++ project.c
@@ -377,3 +377,8 @@ void deleteByValue(int data) {
      if (temp->prev != NULL)
         temp->prev->next = temp->next;
     else
+	     head = temp->next;
+    if (temp->next != NULL)
+        temp->next->prev = temp->prev;
+    free(temp);
+}
