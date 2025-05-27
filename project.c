86d6c88d5f397566788376ea732a7d9c75241f82
muskan2205 <muskan.kumari22@s.amity.edu>
2025-05-27 16:04:54 +0530

    i have added few lines

--- project.c
+++ project.c
@@ -5,4 +5,13 @@ struct Node {
     struct Node* next;
 };
 struct Node* head = NULL;
-
+void insertAtBeginning(int);
+void insertAtEnd(int);
+void insertAfter(int, int);
+void deleteByValue(int);
+void deleteAtBeginning();
+void deleteAtEnd();
+void search(int);
+void display();
+int countNodes();
+void reverseList();
